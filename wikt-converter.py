3b0dc935adc2abe671197c6ed542563f24fa87db
Vesihiisi <alicia@fagerving.se>
2016-11-25 09:38:30 +0100

    start working on parsing wikt articles

--- wikt-converter.py
+++ wikt-converter.py
@@ -1,13 +1,40 @@
 import json
 import xmltodict
+import mwparserfromhell
 
-xmlFile = "small.xml"
-with open("small.xml", "r") as fd:
-    doc=xmltodict.parse(fd.read())
+def xmlToDict(filename):
+    with open(filename, "r") as fd:
+        doc=xmltodict.parse(fd.read())
+    return doc
 
-with open("result.json", "w") as fp:
-    fp.write(json.dumps(doc, indent=4, sort_keys=False))
+def saveDictAsJsonFile(doc, filename):
+    with open(filename, "w") as fp:
+        fp.write(json.dumps(doc, indent=4, sort_keys=False))
 
-for page in doc["mediawiki"]["page"]:
-    if page["ns"] == "0":
-        print(page["title"])
+def printMainspaceTitles(doc):
+    for page in doc["mediawiki"]["page"]:
+        if page["ns"] == "0":
+            print(page["title"])
+
+def getTextByTitle(doc, pagetitle):
+    for page in doc["mediawiki"]["page"]:
+        if page["title"] == pagetitle:
+            return page["revision"]["text"]["#text"]
+
+def getSectionInLanguage(article, language):
+    parsed = mwparserfromhell.parse(article)
+    return parsed.get_sections(levels=[2], matches=language)
+
+def getArticlesInLanguage(doc, language):
+    articles = []
+    for page in doc["mediawiki"]["page"]:
+        if page["ns"] == "0":
+            parsed = getSectionInLanguage(page["revision"]["text"]["#text"], language)
+            if len(parsed) > 0:
+                articles.append(parsed)
+    return articles
+
+if __name__ == "__main__":
+    doc = xmlToDict("small.xml")
+    swedishArticles = getArticlesInLanguage(doc, "Svenska")
+    print(swedishArticles[0][0])
