a830a74879f18298351957aa847925609efbbea4
Vesihiisi <alicia@fagerving.se>
2016-11-26 12:39:05 +0100

    flesh out models

--- item.py
+++ item.py
@@ -2,11 +2,19 @@ class Citation(EmbeddedDocument()):
 	author = StringField(required=True)
 	content = StringField(required=True)
 	date = DateTimeField(required=True)
+	tags = ListField(StringField(), required=False)
 	url = URLField(required=False)
 	work = StringField(required=False)
 
+class Meaning(EmbeddedDocument()):
+	cites = ListField(Citation(), required=False)
+	definition = StringField(required=True)
+	examples = ListField(StringField(), required=False)
+	order = IntField(required=True)
+
 class DictItem(Document):
-	lemma_id = StringField(required=True)
+	item_id = StringField(required=True)
 
 class Lemma(DictItem):
-	tags = ListField(StringField(required=False))
+	tags = ListField(StringField(), required=False)
+	pos = StringField(required=True)
